8fd6f978f362fdacb8074ffe243fc4e484e7f4b8
Vincent Collis <vinsanity0050@gmail.com>
2021-07-03 02:10:10 -0400

    adding bet spread function

---
+++ src/components/Spread.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const CardButton = styled.button`
+    background-color: green;
+    margin: 5px;
+`
+const CardInputBox = styled.input`
+    border: black solid;
+    width: 25px;
+`
+
+export default function Spread() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
