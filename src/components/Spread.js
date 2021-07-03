003e82cdb29682201ab70cd6b4bc4c87f2fac008
Vincent Collis <vinsanity0050@gmail.com>
2021-07-03 02:18:34 -0400

    adding buttons

--- src/components/Spread.js
+++ src/components/Spread.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const CardButton = styled.button`
+const SpreadButton = styled.button`
     background-color: green;
     margin: 5px;
 `
@@ -11,9 +11,24 @@ const CardInputBox = styled.input`
 `
 
 export default function Spread() {
+
+    // save bet spread
+    let oneToEight = {name: "1-8", value:1}
+    let oneToTen = {name: "1-10", value:2}
+    let oneToTwelve = {name: "1-12", value:3}
+
+    // save all cards to on object array
+    let betSpread = [oneToEight,oneToTen,oneToTwelve]
+
     return (
         <div>
-            
+              {
+                betSpread.map((obj, i) => 
+                    <SpreadButton key={i} data-value={obj.value} onClick={updateHandlerClick} onKeyPress={handleKeyPress}> 
+                        {obj.name} 
+                    </SpreadButton>
+                )
+            }
         </div>
     )
 }
